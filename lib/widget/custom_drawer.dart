1574ee87cd486b9c4115b9335e6cf797f65d6856
abotarek74 <moustafatarek013@gmail.com>
2025-01-12 19:59:57 +0200

    Create all expenses widget

--- lib/widget/custom_drawer.dart
+++ lib/widget/custom_drawer.dart
@@ -32,7 +32,7 @@ class CustomDrawer extends StatelessWidget {
             child: Column(
               children: [
                 Expanded(
-                  child: SizedBox(),
+                  child: SizedBox(height: 20),
                 ),
                 InActiveDrawerItem(
                     drawerItemsModel: DrawerItemsModel(
